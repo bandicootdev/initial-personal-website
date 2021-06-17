86135d17bc54031995804975180e50f7fd5fc7e9
Thaymer Portillo <thaymerapv@gmail.com>
2021-06-17 12:24:57 -0400

    changes in routes client

--- client/src/config/routes.js
+++ client/src/config/routes.js
@@ -1,8 +1,11 @@
 import LayoutAdmin from "../layouts/LayoutAdmin";
-
+import LayoutBasic from "../layouts/LayoutBasic";
 import AdminHome from "../pages/Admin";
 
 import AdminSignIn from "../pages/Admin/SignIn";
+
+import Home from '../pages/Home';
+import Contact from "../pages/Contact";
 
 const routes = [
     {
@@ -19,6 +22,21 @@ const routes = [
                 path: '/admin/login',
                 component: AdminSignIn,
                 exact: true,
+            }
+        ]
+    }, {
+        path: '/',
+        component: LayoutBasic,
+        exact: false,
+        routes: [
+            {
+                path: '/',
+                component: Home,
+                exact: true
+            }, {
+                path: '/contact',
+                component: Contact,
+                exact: true
             }
         ]
     }
--- client/src/layouts/LayoutBasic.js
+++ client/src/layouts/LayoutBasic.js
@@ -1,12 +1,30 @@
 import React from "react";
+import {Route} from 'react-router-dom'
 import {Layout} from "antd";
+import {Content, Footer} from "antd/es/layout/layout";
 
-export default function LayoutBasic() {
+import './LayoutBasic.scss';
+
+
+export default function LayoutBasic(props) {
+    const {routes} = props;
     return (
         <Layout>
             <h2>Menu Side</h2>
-            <div>Content</div>
-            <h2>Footer</h2>
+            <Layout>
+                <Content>
+                    <LoadRouters routes={routes}/>
+                </Content>
+                <Footer>
+                    ay no
+                </Footer>
+            </Layout>
         </Layout>
     )
+}
+
+function LoadRouters({routes}) {
+    return routes.map((route, index) => (
+        <Route key={index} path={route.path} exact={route.exact} component={route.component}/>
+    ))
 }
