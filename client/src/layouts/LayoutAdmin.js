52ca1f8c3eb617eb39fdcde7014ec61e60883bf0
Thaymer Portillo <thaymerapv@gmail.com>
2021-06-17 11:36:58 -0400

    changes in routes admin

--- client/src/layouts/LayoutAdmin.js
+++ client/src/layouts/LayoutAdmin.js
@@ -1,14 +1,39 @@
 import React from "react";
+import {Route} from 'react-router-dom';
 import {Layout} from "antd";
 
-export default function LayoutAdmin() {
+import './LayoutAdmin.scss';
+import {Content, Footer, Header} from "antd/es/layout/layout";
+
+export default function LayoutAdmin(props) {
+    const {routes} = props;
+    console.log(props)
     return (
         <Layout>
             <h2>Menu Side</h2>
-            <div>
-                contenidoo...
-            </div>
-            <h5>Footer...</h5>
+            <Layout>
+                <Header>
+                    Header
+                </Header>
+                <Content>
+                    <LoadRouters routes={routes}/>
+                </Content>
+                <Footer>
+                    Footer
+                </Footer>
+            </Layout>
+
         </Layout>
     )
+}
+
+function LoadRouters({routes}) {
+    return routes.map((route, index) => (
+        <Route
+            key={index}
+            path={route.path}
+            exact={route.exact}
+            component={route.component}
+        />
+    ))
 }
